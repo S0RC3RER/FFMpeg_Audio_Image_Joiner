4be1a628a5fd67b5a476e59f8fda3b1cc1d16bcb
TheMrNomnom <52754734+TheMrNomnom@users.noreply.github.com>
2023-03-13 16:33:28 -0400

    create new pyqt6 version

--- main.py
+++ main.py
@@ -1,118 +1,94 @@
-import tkinter as tk
-import tkinter.filedialog as filedialog
-import tkinter.ttk as ttk
-import tkinter.messagebox as messagebox
-import ffmpeg
-from PIL import Image, ImageTk
+import os
+from PyQt6.QtWidgets import QApplication, QFileDialog, QLabel, QVBoxLayout, QWidget, QPushButton, QHBoxLayout, \
+    QListWidget
 
 
-# TODO - Modify this function to use new implementation
-def combine_audio_image(image_file, audio_files, output_file):
-    audio_inputs = [ffmpeg.input(f) for f in audio_files]
-    (
-        ffmpeg
-        .concat(*audio_inputs, v=1, a=1)
-        .input(image_file, loop=1)
-        .output(output_file, **{'c:v':'libx264'}, tune='stillimage', **{'c:a': 'aac'}, b='192k', pix_fmt='yuv420p', shortest=True)
-        .global_args('-loglevel', 'error')
-        .global_args('-report')
-        .run()
-    )
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.image_file = ""
+        self.audio_files = []
+        self.output_file = ""
+        self.initUI()
 
+    def initUI(self):
+        layout = QVBoxLayout()
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
-        self.create_widgets()
+        # Image selection button
+        image_button = QPushButton("Select Image File")
+        image_button.clicked.connect(self.select_image_file)
+        layout.addWidget(image_button)
 
-    def create_widgets(self):
-        self.select_image = tk.Button(self)
-        self.select_image["text"] = "Select Image"
-        self.select_image["command"] = self.select_image_command
-        self.select_image.pack()
+        # Audio selection button
+        audio_button = QPushButton("Select Audio Files")
+        audio_button.clicked.connect(self.select_audio_files)
+        layout.addWidget(audio_button)
 
-        self.image_frame = tk.Frame(self)
-        self.image_frame.pack()
-        self.image_path = tk.Label(self.image_frame)
-        self.image_path.pack(side="left")
-        self.remove_image = tk.Button(self.image_frame)
-        self.remove_image["text"] = "Remove"
-        self.remove_image["command"] = self.remove_image_command
-        self.remove_image.pack(side="right")
+        # Output selection button
+        output_button = QPushButton("Select Output File")
+        output_button.clicked.connect(self.select_output_file)
+        layout.addWidget(output_button)
 
-        self.image_preview = tk.Label(self)
-        self.image_preview.pack()
+        # Image path label
+        self.image_label = QLabel("No image file selected.")
+        layout.addWidget(self.image_label)
 
-        self.select_audio = tk.Button(self)
-        self.select_audio["text"] = "Select Audio"
-        self.select_audio["command"] = self.select_audio_command
-        self.select_audio.pack()
+        # Audio files list
+        audio_label = QLabel("Audio files:")
+        layout.addWidget(audio_label)
+        self.audio_list = QListWidget()
+        layout.addWidget(self.audio_list)
 
-        self.audio_list = tk.Listbox(self)
-        self.audio_list.pack(fill="both", expand=True)
-        self.audio_list.bind("<Double-Button-1>", self.remove_audio_command)
+        # Combine button
+        combine_button = QPushButton("Combine")
+        combine_button.clicked.connect(self.combine)
+        layout.addWidget(combine_button)
 
-        self.remove_audio = tk.Button(self)
-        self.remove_audio["text"] = "Remove Audio"
-        self.remove_audio["command"] = self.remove_audio_command
-        self.remove_audio.pack()
+        self.setLayout(layout)
+        self.show()
 
-        self.combine = tk.Button(self)
-        self.combine["text"] = "Combine"
-        self.combine["command"] = self.combine_command
-        self.combine.pack()
+    def select_image_file(self):
+        image_file, _ = QFileDialog.getOpenFileName(self, "Select Image File", "",
+                                                    "Image Files (*.jpg *.jpeg *.png *.bmp)")
+        if image_file:
+            self.image_file = image_file
+            self.image_label.setText(image_file)
 
-        self.progress = ttk.Progressbar(self, orient="horizontal", length=200, mode="indeterminate")
-        self.progress.pack()
+    def select_audio_files(self):
+        audio_files, _ = QFileDialog.getOpenFileNames(self, "Select Audio Files", "", "Audio Files (*.mp3 *.wav *.ogg)")
+        if audio_files:
+            self.audio_files = audio_files
+            self.audio_list.clear()
+            for audio_file in audio_files:
+                self.audio_list.addItem(audio_file)
 
-    def select_image_command(self):
-        self.image_file = filedialog.askopenfilename(initialdir=".", title="Select Image", filetypes=(
-            ("Image files", "*.jpg;*.png;*.jpeg"), ("All files", "*.*")))
-        if self.image_file:
-            self.image_path["text"] = self.image_file
-            image = Image.open(self.image_file)
-            image = image.resize((300, 300), Image.ANTIALIAS)
-            image = ImageTk.PhotoImage(image)
-            self.image_preview["image"] = image
-            self.image_preview.photo = image
+    def select_output_file(self):
+        output_file, _ = QFileDialog.getSaveFileName(self, "Save Output File", "", "Video Files (*.mp4)")
+        if output_file:
+            self.output_file = output_file
 
-    def remove_image_command(self):
-        self.image_file = None
-        self.image_path["text"] = ""
-        self.image_preview["image"] = ""
-
-    def select_audio_command(self):
-        audio_file = filedialog.askopenfilename(initialdir=".", title="Select Audio", multiple=True, filetypes=(
-            ("Audio files", "*.wav;*.mp3;*.aac"), ("All files", "*.*")))
-        if audio_file:
-            for file in audio_file:
-                self.audio_list.insert("end", file)
-
-    def remove_audio_command(self, event=None):
-        selected = self.audio_list.curselection()
-        for index in selected:
-            self.audio_list.delete(index)
-
-    def combine_command(self):
+    def combine(self):
         if not self.image_file:
-            messagebox.showerror("Error", "Please select an image.")
+            self.image_label.setText("No image file selected.")
             return
-        if self.audio_list.size() == 0:
-            messagebox.showerror("Error", "Please select audio files.")
+        if not self.audio_files:
+            self.audio_list.addItem("No audio files selected.")
             return
-        audio_files = [self.audio_list.get(i) for i in range(self.audio_list.size())]
-        output_file = filedialog.asksaveasfilename(initialdir=".", title="Save Video",
-                                                   filetypes=(("Video files", "*.mp4"), ("All files", "*.*")),
-                                                   defaultextension=".mp4")
-        if not output_file:
+        if not self.output_file:
             return
-        self.progress.start()
-        combine_audio_image(self.image_file, audio_files, output_file)
-        self.progress.stop()
-        messagebox.showinfo("Info", "Video created successfully.")
+        combine_audio_and_image(self.image_file, self.audio_files, self.output_file)
 
 
-if __name__ == "__main__":
-    app = Application()
-    app.mainloop()
+def combine_audio_and_image(image_file, audio_files, output_file):
+    audio_str = ""
+    for audio_file in audio_files:
+        audio_str += f"-i {audio_file} "
+    cmd = f"ffmpeg -loop 1 -i {image_file} {audio_str} -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt " \
+          f"yuvj420p -shortest {output_file}"
+    os.system(cmd)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = MainWindow()
+    app.exec()
