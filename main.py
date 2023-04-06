a7a7f6b36b6e89a162309eb75eb919666a30fa87
TheMrNomnom <52754734+TheMrNomnom@users.noreply.github.com>
2023-04-05 22:15:53 -0400

    add basic docstrings

--- main.py
+++ main.py
@@ -7,11 +7,28 @@ from PyQt6.QtWidgets import QApplication, QFileDialog, QLabel, QVBoxLayout, QWid
 
 
 class ImageAudioCombiner(QThread):
+    """
+    A subclass of QThread that defines a thread for combining an image and one or more audio files into MP4 video files.
+    The thread takes an image file, a queue of audio files, and an output folder as inputs. The run method of the
+    thread iteratively pops an audio file from the queue and combines it with the image file using FFmpeg. The progress
+    of the thread is reported as an integer between 0 and 100 through the progress_signal signal. When the thread
+    finishes, the finished_signal signal is emitted. If an error occurs during the process, the error_signal signal
+    is emitted with a message describing the error.
+    """
     progress_signal = pyqtSignal(int)
     finished_signal = pyqtSignal()
     error_signal = pyqtSignal(str)
 
     def __init__(self, image_file, audio_files, output_folder, parent=None):
+        """
+        :param image_file: Path to image file
+        :type image_file: basestring
+        :param audio_files: Path to audio file
+        :type audio_files: basestring
+        :param output_folder: Path to output folder
+        :type output_folder: basestring
+        :param parent:
+        """
         super().__init__(parent)
         self.image_file = image_file
         self.audio_files = audio_files
@@ -19,12 +36,24 @@ class ImageAudioCombiner(QThread):
         self.paused = False
 
     def pause(self):
+        """
+        Pause the combination
+        :return:
+        """
         self.paused = True
 
     def unpause(self):
+        """
+        Unpause the combination
+        :return:
+        """
         self.paused = False
 
     def run(self):
+        """
+        # TODO
+        :return:
+        """
         if not os.path.exists(self.output_folder):
             try:
                 os.makedirs(self.output_folder)
@@ -70,6 +99,13 @@ class ImageAudioCombiner(QThread):
 
 
 class MainWindow(QWidget):
+    """
+    A subclass of QWidget that defines the main window of the GUI application. The window contains several buttons and
+    labels for selecting and displaying the image file, audio files, and output folder, as well as for starting,
+    canceling, and monitoring the progress of the image and audio file combination process. The class also defines
+    several methods for handling user interactions with the GUI components, such as selecting and removing files,
+    updating the state of buttons, and launching and canceling the image and audio file combination thread.
+    """
     def __init__(self):
         super().__init__()
         self.image_file = ""
@@ -139,6 +175,11 @@ class MainWindow(QWidget):
         self.show()
 
     def update_audio_remove_button(self):
+        """
+        A method of the MainWindow class that updates the state of the "Remove Selected Audio Files" button based on
+        whether any audio files are currently selected in the audio file list.
+        :return: None
+        """
         selected_items = self.audio_list.selectedItems()
         if selected_items:
             self.audio_remove_button.setEnabled(True)
@@ -146,11 +187,21 @@ class MainWindow(QWidget):
             self.audio_remove_button.setEnabled(False)
 
     def remove_image_file(self):
+        """
+        A method of the MainWindow class that removes the currently selected image file and updates the corresponding
+        label and button state accordingly.
+        :return: None
+        """
         self.image_file = ""
         self.image_label.setText("No image file selected.")
         self.image_remove_button.setEnabled(False)
 
     def remove_selected_audio_files(self):
+        """
+        A method of the MainWindow class that removes the currently selected audio files from the audio file list and
+        updates the corresponding button state accordingly.
+        :return:
+        """
         selected_items = self.audio_list.selectedItems()
         for item in selected_items:
             self.audio_files.remove(item.text())
@@ -158,14 +209,27 @@ class MainWindow(QWidget):
         self.update_audio_remove_button()
 
     def select_image_file(self):
+        """
+        A method of the MainWindow class that opens a file dialog for selecting an image file and updates the
+        corresponding label and button state accordingly.
+        :return:
+        """
         image_file, _ = QFileDialog.getOpenFileName(self, "Select Image File", "",
                                                     "Image Files (*.jpg *.jpeg *.png *.bmp)")
         if image_file:
+            if not os.path.isfile(image_file):
+                QMessageBox.critical(self, "Error", "Invalid image file path.")
+                return
             self.image_file = image_file
             self.image_label.setText(image_file)
             self.image_remove_button.setEnabled(True)
 
     def select_audio_files(self):
+        """
+        A method of the MainWindow class that opens a file dialog for selecting one or more audio files and updates the
+        corresponding list and button state accordingly.
+        :return:
+        """
         audio_files, _ = QFileDialog.getOpenFileNames(self, "Select Audio Files", "", "Audio Files (*.mp3 *.wav *.ogg)")
         if audio_files:
             self.audio_files.extend(audio_files)
@@ -173,11 +237,22 @@ class MainWindow(QWidget):
                 self.audio_list.addItem(audio_file)
 
     def select_output_folder(self):
+        """
+        A method of the MainWindow class that opens a file dialog for selecting an output folder for the combined video
+        files and updates the corresponding label and button state accordingly.
+        :return:
+        """
         output_folder, _ = QFileDialog.getExistingDirectory(self, "Save Output Folder", "")
         if output_folder:
             self.output_folder = output_folder
 
     def combine(self):
+        """
+        A method of the MainWindow class that creates an instance of the ImageAudioCombiner thread with the selected
+        image file, audio files, and output folder as inputs, connects its signals to the corresponding GUI components,
+        and starts the thread.
+        :return:
+        """
         if not self.image_file:
             self.image_label.setText("No image file selected.")
             return
@@ -206,6 +281,11 @@ class MainWindow(QWidget):
         self.thread.start()
 
     def cancel(self):
+        """
+        A method of the MainWindow class that sets a flag to cancel the currently running ImageAudioCombiner thread,
+        disables the "Combine" button, and enables the "Cancel" button.
+        :return: None
+        """
         self.cancel_button.setEnabled(False)
         self.pause_button.setEnabled(False)
         self.combine_button.setEnabled(True)
@@ -218,9 +298,17 @@ class MainWindow(QWidget):
         self.thread.pause()
 
     def update_progress_label(self, progress):
+        """
+        :param progress: progress percentage
+        :type progress: int
+        :return: None
+        """
         self.progress_label.setText(f"Progress: {progress}%")
 
     def pause(self):
+        """
+        :return:None
+        """
         if not self.combiner_thread:
             return
 
@@ -232,6 +320,9 @@ class MainWindow(QWidget):
             self.pause_button.setText("Pause")
 
     def combine_finished(self):
+        """
+        :return:
+        """
         self.combine_button.setEnabled(True)
         self.pause_button.setEnabled(False)
 
@@ -244,16 +335,12 @@ class MainWindow(QWidget):
         self.progress_label.setText("Progress: 100%")
 
     def handle_error(self, message):
+        """
+        :param message: error message
+        :type message: basestring
+        :return:
+        """
         QMessageBox.warning(self, "Error", message)
-
-
-# def combine_audio_and_image(image_file, audio_files, output_folder):
-#     audio_str = ""
-#     for audio_file in audio_files:
-#         audio_str += f"-i {audio_file} "
-#     cmd = f"ffmpeg -loop 1 -i {image_file} {audio_str} -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt " \
-#           f"yuvj420p -shortest {output_folder}"
-#     os.system(cmd)
 
 
 if __name__ == '__main__':
